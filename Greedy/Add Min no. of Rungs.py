b3b2a2fc27bc3865dbda8c260ce1d2afad23c4ea
Debajyoti_Roy_Chowdhury <63442163+debajyotiroyc@users.noreply.github.com>
2022-03-07 18:35:39 +0530

    Add files via upload

---
+++ Greedy/Add Min no. of Rungs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def addRungs(self, rungs: List[int], dist: int) -> int:
+      k,a=0,0
+      i=0
+      while i<len(rungs):
+        if (rungs[i]-a)<=dist:
+          pass
+        else:
+          if (rungs[i]-a)%dist==0:
+            k=k+((rungs[i]-a)//dist)-1
+          else:
+            k=k+((rungs[i]-a)//dist)
+        a=rungs[i]
+        i=i+1
+      return k
+          
+      
+        
